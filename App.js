d61c17d20a9ccbfca863247d9284a337bb62eb06
Francisco <javiermartincuenca@gmail.com>
2019-06-14 22:41:23 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Header } from './src/components';
+
+export default function App() {
+  return (
+    <View>
+      <Header/>
+    </View>
+  );
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+App for track crypto currency
+
+Original article
+https://medium.com/free-code-camp/learn-how-to-build-a-rn-redux-cryptocurrency-app-chapter-iv-b0e0c5ca2dca
